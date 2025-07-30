10d091fd2cf47a8154782da241c973dce44345d1
Abdurahman sualih <156515083+abudi-09@users.noreply.github.com>
2025-07-30 21:16:37 +0300

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js"; // Adjust the path as necessary
+import { signup, login, logout,getMe } from "../controllers/auth.controller.js"; // Adjust the path as necessary
+import { protectRoute } from "../middleware/protectRoute.js";
 //import { protectRoute } from "../middleware/protectRoute.js"; // Adjust the path as necessary
 const router = express.Router();
 //router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/me", protectRoute, getMe);
 export default router;
